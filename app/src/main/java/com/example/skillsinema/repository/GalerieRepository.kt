01aee61773b96a8592a0a5cca45a7bf0232f7b25
Andrey <phobos05@bk.ru>
2023-07-12 23:55:39 +0700

    12_07_23 1

--- app/src/main/java/com/example/skillsinema/repository/GalerieRepository.kt
+++ app/src/main/java/com/example/skillsinema/repository/GalerieRepository.kt
@@ -2,6 +2,7 @@ package com.example.skillsinema.repository
 
 
 
+import android.util.Log
 import com.example.skillsinema.entity.Model
 import com.example.skillsinema.entity.ModelGalerie
 import com.example.skillsinema.entity.ModelStaff
@@ -11,6 +12,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -33,8 +38,9 @@ class GalerieRepository @Inject constructor() {
 
 
     @Provides
-    suspend fun getGalerie(): List<ModelGalerie.Item>? {
-        return retrofitGalerie.galerie(301,"STILL", 1).body()
+    suspend fun getGalerie(): List<ModelGalerie.Item> {
+       // parseJSON()
+        return retrofitGalerie.galerie(301,"STILL", 1).items.toList()
 
 
     }
@@ -93,10 +99,62 @@ class GalerieRepository @Inject constructor() {
             @Path(value ="id") id:Int,
             @Query("type") type: String,
             @Query("page") page:Int
-        ): Response<List<ModelGalerie.Item>>
+        ): ModelGalerie
     }
 
     private companion object {
         private const val api_key = "63101d70-3149-4782-99f8-dd1ba5fc4ab1"
     }
+
+
+    /*fun parseJSON() {
+
+        // Create Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        // Create Service
+
+        val service = retrofit.create(ApiInterfaceGalerie::class.java)
+        CoroutineScope(Dispatchers.IO).launch {
+
+            // Do the GET request and get response
+            val response = service.galerie(301,"STILL", 1)
+
+            withContext(Dispatchers.Main) {
+                if (!response.isEmpty()) {
+
+                    val items = response
+                    if (items != null) {
+                        for (i in 0 until items.count()) {
+                            // ID
+                            *//*val id = items[i].staffId ?: "N/A"
+                            Log.d("REPOSTAFF: ", id as String)*//*
+
+                            // Employee Name
+                            val employeeName = items[i].imageUrl ?: "N/A"
+                            Log.d("REPOSTAFF: ", employeeName)
+
+                            // Employee Salary
+                            val employeeSalary = items[i].imageUrl ?: "N/A"
+                            Log.d("REPOSTAFF: ", employeeSalary)
+
+                            // Employee Age
+                            val employeeAge = items[i].imageUrl ?: "N/A"
+                            Log.d("REPOSTAFF: ", employeeAge)
+
+                        }
+                    }
+
+                } else {
+
+                    Log.e("RETROFIT_ERROR", response.code().toString())
+
+                }
+            }
+        }
+    }*/
+
 }
