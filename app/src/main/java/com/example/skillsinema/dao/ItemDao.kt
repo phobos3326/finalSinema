51a705d80e728ca04c09ea848412bf976ba6de16
Andrey <phobos05@bk.ru>
2023-08-14 20:49:53 +0700

    14.06.23 onConfict

--- app/src/main/java/com/example/skillsinema/dao/ItemDao.kt
+++ app/src/main/java/com/example/skillsinema/dao/ItemDao.kt
@@ -2,9 +2,14 @@ package com.example.skillsinema.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface ItemDao {
-    @Insert(entity = ItemFilm::class)
+    @Insert(entity = ItemFilm::class, onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(item: ItemFilm)
+
+    @Update
+    suspend fun update(item: ItemFilm)
 }
--- app/src/main/java/com/example/skillsinema/dao/ItemRepository.kt
+++ app/src/main/java/com/example/skillsinema/dao/ItemRepository.kt
@@ -2,14 +2,20 @@ package com.example.skillsinema.dao
 
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 import javax.inject.Inject
 
 class ItemRepository @Inject constructor(
 private val itemDao: ItemDao
 ) {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //@Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertItem(itemFilm: ItemFilm) {
         itemDao.insert(itemFilm)
+    }
+
+   // @Update
+    suspend fun update(itemFilm: ItemFilm){
+        itemDao.update(itemFilm)
     }
 
     // Other repository functions
