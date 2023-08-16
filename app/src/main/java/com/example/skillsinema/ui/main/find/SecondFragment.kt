e3bcad83cb7bbd7cdbd619ab115bafcf3ea90baa
Andrey <phobos05@bk.ru>
2023-08-16 12:16:59 +0700

    16.06.23 search bar

--- app/src/main/java/com/example/skillsinema/ui/main/find/SecondFragment.kt
+++ app/src/main/java/com/example/skillsinema/ui/main/find/SecondFragment.kt
@@ -5,11 +5,80 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.skillsinema.R
 import com.example.skillsinema.databinding.FragmentGalerieBinding
 import com.example.skillsinema.databinding.FragmentSecondBinding
@@ -53,15 +122,102 @@ class SecondFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             // Dispose of the Composition when the view's LifecycleOwner
             // is destroyed
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            //  setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 MaterialTheme {
-                    // In Compose world
-                    Text("Hello Compose!")
+
+                    Column(
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                    ) {
+                        SearchBar1()
+                    }
                 }
             }
+
+
         }
     }
+
+
+    @Composable
+    fun SearchBar1() {
+        var query by rememberSaveable { mutableStateOf("") }
+
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp),
+               // .padding(horizontal =  16.dp),
+
+            value = query,
+            onValueChange = { query = it },
+            placeholder = { Text("Фильмы, актеры, режисёры") },
+            leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+            trailingIcon = {
+                IconButton(
+                    onClick = { query = "" },
+                    content = {
+                        Icon(
+                            painterResource(R.drawable.find_settings),
+                            contentDescription = null
+                        )
+                    }
+                )
+            },
+            singleLine = true,
+            shape = MaterialTheme.shapes.extraLarge,
+            colors = TextFieldDefaults.textFieldColors(
+
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                //cursorColor = MaterialTheme.colors.onSurface
+            ),
+            //keyboardActions = KeyboardActions(onSearch = { onQueryChanged(query) }),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
+        )
+    }
+
+    @Preview(showBackground = true)
+
+    @Composable
+    fun preview() {
+        SearchBar1()
+    }
+
+    @Composable
+    fun SearchBar() {
+        var text by remember { mutableStateOf("") }
+        Box(
+            modifier = Modifier
+                .width(350.dp)
+                .height(50.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(100.dp))
+                .padding(10.dp, 20.dp)
+        ) {
+
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Фильмы, актеры, режисёры") },
+                leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+                trailingIcon = {
+                    Icon(
+                        painterResource(R.drawable.find_settings),
+                        contentDescription = null
+                    )
+                },
+
+                modifier = Modifier
+                    .width(350.dp)
+                    .height(50.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(20.dp))
+                    .fillMaxSize(1f)
+
+
+            )
+        }
+    }
+
 
     companion object {
         /**
