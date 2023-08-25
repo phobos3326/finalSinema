2004185c7ea61175cf49a3d872e0406964e4a10b
Andrey <phobos05@bk.ru>
2023-08-26 02:08:58 +0700

    23.06.23 SearchScreen -> options

--- app/src/main/java/com/example/skillsinema/ui/main/find/SearchOptionsScreen.kt
+++ app/src/main/java/com/example/skillsinema/ui/main/find/SearchOptionsScreen.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.annotation.ColorRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,21 +19,37 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.RangeSlider
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Slider
+import androidx.compose.material.SliderDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -39,7 +58,7 @@ import kotlin.reflect.jvm.internal.impl.resolve.constants.KClassValue
 
 @Composable
 fun SearchOptionsScreen(
-    viewModel: SearchViewmodel ,
+    viewModel: SearchViewmodel,
     navController: NavHostController
 ) {
     Text(text = "parametr screen")
@@ -60,14 +79,6 @@ fun SegmentedControlPage(viewModel: SearchViewmodel) {
                 .padding(30.dp)
                 .background(Color.White),
         ) {
-            /**
-             * Wrap size
-             */
-
-
-            /**
-             * Multiple items
-             */
 
             val items1 = listOf("Все", "Фильмы", "Сериалы")
             SegmentedControl(
@@ -84,9 +95,11 @@ fun SegmentedControlPage(viewModel: SearchViewmodel) {
                     0 -> {
                         viewModel.uiState.value.filmType = "ALL"
                     }
+
                     1 -> {
                         viewModel.uiState.value.filmType = "FILM"
                     }
+
                     2 -> {
                         viewModel.uiState.value.filmType = "TV_SERIES"
                     }
@@ -95,7 +108,6 @@ fun SegmentedControlPage(viewModel: SearchViewmodel) {
         }
     }
 }
-
 
 
 @Composable
@@ -214,10 +226,205 @@ fun SegmentedControl(
     }
 }
 
-@Preview()
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Preview(viewModel: SearchViewmodel = viewModel()){
-//@PreviewParameter()
-    //SegmentedControl()
-    SegmentedControlPage(viewModel = viewModel)
+fun ButtonRow() {
+    Column {
+        Row(
+            modifier = Modifier
+                .clickable { }
+                .border(width = 1.dp, color = Color(0x4DB5B5C9))
+                .width(360.dp)
+                .height(50.dp)
+                .padding(start = 26.dp, top = 16.dp, end = 26.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = "Страна",
+                style = TextStyle(
+                    fontSize = 16.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF272727),
+                    textAlign = TextAlign.Center,
+                )
+            )
+            Text(
+                text = "Россия",
+                style = TextStyle(
+                    fontSize = 14.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF838390),
+                    textAlign = TextAlign.End,
+                )
+            )
+        }
+        Row(
+            modifier = Modifier
+                .clickable { }
+                .border(width = 1.dp, color = Color(0x4DB5B5C9))
+                .width(360.dp)
+                .height(50.dp)
+                .padding(start = 26.dp, top = 16.dp, end = 26.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = "Жанр",
+                style = TextStyle(
+                    fontSize = 16.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF272727),
+                    textAlign = TextAlign.Center,
+                )
+            )
+            Text(
+                text = "Комедия",
+                style = TextStyle(
+                    fontSize = 14.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF838390),
+                    textAlign = TextAlign.End,
+                )
+            )
+        }
+        Row(
+            modifier = Modifier
+                .clickable { }
+                .border(width = 1.dp, color = Color(0x4DB5B5C9))
+                .width(360.dp)
+                .height(50.dp)
+                .padding(start = 26.dp, top = 16.dp, end = 26.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = "Год",
+                style = TextStyle(
+                    fontSize = 16.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF272727),
+                    textAlign = TextAlign.Center,
+                )
+            )
+            Text(
+                text = "с 1998 до 2017",
+                style = TextStyle(
+                    fontSize = 14.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF838390),
+                    textAlign = TextAlign.End,
+                )
+            )
+        }
+        Row(
+            modifier = Modifier
+
+               // .border(width = 1.dp, color = Color(0x4DB5B5C9))
+                .width(360.dp)
+                .height(50.dp)
+                .padding(start = 26.dp, top = 16.dp, end = 26.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = "Рейтинг",
+                style = TextStyle(
+                    fontSize = 16.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF272727),
+                    textAlign = TextAlign.Center,
+                )
+            )
+            Text(
+                text = "любой",
+                style = TextStyle(
+                    fontSize = 14.sp,
+
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF838390),
+                    textAlign = TextAlign.End,
+                )
+            )
+        }
+        Row(
+            modifier = Modifier
+
+               // .border(width = 1.dp, color = Color(0x4DB5B5C9))
+                .width(360.dp)
+                .height(50.dp)
+                .padding(start = 26.dp, top = 16.dp, end = 26.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        )
+
+        {
+
+            Column {
+                Row{
+                    var sliderPosition by remember { mutableStateOf(0f..100f) }
+                    RangeSlider(
+                        modifier = Modifier.semantics { contentDescription = "Localized Description" },
+                        value = sliderPosition,
+                        onValueChange = { sliderPosition = it },
+                        valueRange = 0f..100f,
+                        steps = 10,
+                        onValueChangeFinished = {
+                            // launch some business logic update with the state you hold
+                            // viewModel.updateSelectedSliderValue(sliderPosition)
+                        },
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Top,
+
+                    modifier = Modifier
+                        .width(295.dp)
+                        .height(40.dp)
+                        .border(width = 1.dp, color = Color(0x4DB5B5C9))
+                ) {
+                    Text(
+                        text = "0",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF272727),
+                            textAlign = TextAlign.Center,
+                        )
+                    )
+                    Text(
+                        text = "10",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF838390),
+                            textAlign = TextAlign.End,
+                        )
+                    )
+                }
+
+            }
+
+
+        }
+
+    }
 }
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+fun Preview() {
+
+    ButtonRow()
+}
+
