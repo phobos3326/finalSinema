c0c0abd9ee86e10bdd449452b84dbc732cdd7510
Andrey <phobos05@bk.ru>
2023-12-03 23:43:32 +0700

    02.12.23 db

--- app/src/main/java/com/example/skillsinema/ui/main/profile/ThirdFragment.kt
+++ app/src/main/java/com/example/skillsinema/ui/main/profile/ThirdFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.skillsinema.R
+import com.example.skillsinema.databinding.FragmentItemInfoBinding
+import com.example.skillsinema.databinding.FragmentThirdBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 // TODO: Rename parameter arguments, choose names that match
@@ -32,12 +35,27 @@ class ThirdFragment : Fragment() {
         }
     }
 
+
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+
+    val viewModel: ThirdFragmentViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
+
+        _binding=FragmentThirdBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.createCollectionTextView.setOnClickListener {
+            viewModel.wantToSee()
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/example/skillsinema/ui/main/profile/ThirdFragmentViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.skillsinema.ui.main.profile
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.skillsinema.dao.CollectionDao
+import com.example.skillsinema.dao.CollectionEntityRepository
+import com.example.skillsinema.dao.CollectionsEntity
+import com.example.skillsinema.dao.ItemDao
+import com.example.skillsinema.dao.WantToSeeFilm
+import com.example.skillsinema.dao.WantToSeeFilmRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ThirdFragmentViewModel @Inject constructor(
+    private var wantToSeeFilmRepository: WantToSeeFilmRepository,
+    private var collectionEntityRepository: CollectionEntityRepository,
+    private val itemDao: CollectionDao
+):ViewModel() {
+
+    private val _collection = MutableStateFlow<List<CollectionsEntity>>(emptyList())
+    val collection = _collection.asStateFlow()
+
+init {
+
+
+}
+    fun wantToSee(){
+        viewModelScope.launch(Dispatchers.IO) {
+            val db  = collectionEntityRepository.getAll()
+            _collection.value=db
+
+            //Log.d(TAG, "list ${db}")
+
+            for (i in db){
+                Log.d(TAG, "list ${i.id}, ${i.collection.joinToString(",")}, ${i.collectionName}")
+            }
+
+
+        }
+
+
+    }
+
+
+    companion object{
+        val TAG = "ThirdFragmentViewModel"
+    }
+
+
+}
