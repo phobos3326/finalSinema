7c956102628cdae9622fa986cdfb0d34f4d7b18a
AndreyTyshko <phobos05@bk.ru>
2025-12-22 23:57:22 +0700

    Clean up old architecture files, keep only SOLID structure

--- app/src/main/java/com/example/skillsinema/dao/ext.kt
+++
@@ -1,7 +0,0 @@
-package com.example.skillsinema.dao
-
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-
-inline fun <reified T> Gson.fromJson(json: String) =
-    fromJson<T>(json, object : TypeToken<T>() {}.type)
