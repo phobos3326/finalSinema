ec8c7cce880eec78ac23c6e85675bdb70f458775
AndreyTyshko <phobos05@bk.ru>
2026-01-11 14:54:38 +0700

    06_01_26  25 show all OK!!!

--- app/src/main/java/com/example/skillsinema/domain/usecase/filter/GetFiltersUseCase.kt
+++ app/src/main/java/com/example/skillsinema/domain/usecase/filter/GetFiltersUseCase.kt
@@ -1,19 +1,13 @@
 package com.example.skillsinema.domain.usecase.filter
 
-import com.example.skillsinema.data.model.ModelFilter
+import com.example.skillsinema.domain.model.FiltersResponse
 import com.example.skillsinema.domain.repository.FilterRepository
-
 import javax.inject.Inject
 
 class GetFiltersUseCase @Inject constructor(
     private val repository: FilterRepository
 ) {
-    suspend operator fun invoke(): ModelFilter {
-        val response = repository.getFilters()
-        return if (response.isSuccessful) {
-            response.body()!!
-        } else {
-            throw Exception("Failed to load filters: ${response.message()}")
-        }
+    suspend operator fun invoke(): FiltersResponse {
+        return repository.getFilters()
     }
 }
--- app/src/main/java/com/example/skillsinema/domain/usecase/filter/GetRandomFiltersUseCase.kt
+++ app/src/main/java/com/example/skillsinema/domain/usecase/filter/GetRandomFiltersUseCase.kt
@@ -1,26 +1,25 @@
 package com.example.skillsinema.domain.usecase.filter
 
-import com.example.skillsinema.domain.model.Genre
-import com.example.skillsinema.domain.model.Country
-import com.example.skillsinema.domain.model.FilterParams
 import com.example.skillsinema.domain.repository.FilterRepository
 import javax.inject.Inject
 
+data class RandomFilters(
+    val countryId: Int?,
+    val genreId: Int?
+)
+
 class GetRandomFiltersUseCase @Inject constructor(
-    private val filterRepository: FilterRepository
+    private val repository: FilterRepository
 ) {
-    suspend operator fun invoke(): FilterParams {
-        val filters = filterRepository.getFilters().body()
+    suspend operator fun invoke(): RandomFilters {
+        val filters = repository.getFilters() // уже НЕ Response
 
-        val genres = filters?.genres?.take(17) ?: emptyList()
-        val countries = filters?.countries?.take(34) ?: emptyList()
+        val randomGenreId: Int? = filters.genres.randomOrNull()?.id
+        val randomCountryId: Int? = filters.countries.randomOrNull()?.id
 
-        val randomGenre = genres.randomOrNull()
-        val randomCountry = countries.randomOrNull()
-
-        return FilterParams(
-            countries = randomCountry?.id?.toString(),
-            genres = randomGenre?.id?.toString()
+        return RandomFilters(
+            countryId = randomCountryId,
+            genreId = randomGenreId
         )
     }
 }
